83d65ed78238647ebba0daf168da82f6bd3360d1
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2024-04-21 01:00:06 +0530

    5th Commit by Rashi

--- app/src/main/java/com/example/socialmediaapp/Fragment/RequestFragment.java
+++ app/src/main/java/com/example/socialmediaapp/Fragment/RequestFragment.java
@@ -10,19 +10,11 @@ import android.view.ViewGroup;
 
 import com.example.socialmediaapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RequestFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RequestFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +22,6 @@ public class RequestFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RequestFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RequestFragment newInstance(String param1, String param2) {
         RequestFragment fragment = new RequestFragment();
         Bundle args = new Bundle();
--- app/src/main/java/com/example/socialmediaapp/SignUpActivity.java
+++ app/src/main/java/com/example/socialmediaapp/SignUpActivity.java
@@ -1,4 +1,5 @@
 package com.example.socialmediaapp;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
