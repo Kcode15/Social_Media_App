66de5b3bfcd2ccd35f95842874ee11b167a693c5
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2024-04-21 00:07:37 +0530

    4th Commit by Rashi

--- app/src/main/java/com/example/socialmediaapp/Fragment/SearchFragment.java
+++ app/src/main/java/com/example/socialmediaapp/Fragment/SearchFragment.java
@@ -14,7 +14,6 @@ public class SearchFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     private String mParam1;
     private String mParam2;
 
